994054c32888df158aee1cf810723ba122b8b802
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-11-21 14:19:56 +0200

    Removing some files, before starting the project.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react' // Sovellus ottaa "react" nimisen kirjaston käyttöönsä.
+import ReactDOM from 'react-dom' // Sovellus ottaa "react-dom" nimisen kirjaston käyttöönsä.
+
+import App from './App'
+import './index.css'
+
+ReactDOM.render(<App />, document.getElementById('root'))
